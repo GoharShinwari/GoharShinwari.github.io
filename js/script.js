d4e21c454544a74c293661bfd1046de07c435aed
GoharShinwari <121142075+GoharShinwari@users.noreply.github.com>
2023-01-28 19:34:27 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+function selectCharacter(name) {
+    document.getElementById("selected-character").innerHTML = "You have selected " + name + "!";
+}
+function selectCharacter(character) {
+    // Show the confirmation button
+    document.getElementById("confirmation-button").style.display = "block";
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+    // Display the selected character name
+    document.getElementById("selected-character").innerHTML = "Selected Character: " + character;
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+function cancelSelection() {
+    // Hide the confirmation button
+    document.getElementById("confirmation-button").style.display = "none";
+
+    // Clear the selected character name
+    document.getElementById("selected-character").innerHTML = "";
+}
+
+function hideConfirmation() {
+    document.getElementById("confirmation-button").style.display = "none";
+  }
+
+  function startGame() {
+    // Remove all elements from the HTML body
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  
+    // Create a new canvas element for the game screen
+    var canvas = document.createElement("canvas");
+    canvas.id = "game-canvas";
+    canvas.width = 800;
+    canvas.height = 600;
+    canvas.style.backgroundColor = "black";
+  
+    // Add the canvas to the HTML body
+    document.body.appendChild(canvas);
+  }
+  
